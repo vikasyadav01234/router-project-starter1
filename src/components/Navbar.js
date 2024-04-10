128d5e3d5d7bb8e54804b89676dca8566e2a67f2
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-10 14:39:22 +0530

    main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import logo from "../assets/Logo.svg"
 import {Link} from "react-router-dom"
 import {toast} from "react-hot-toast"
 
-
 const Navbar = (props) => {
     let isLoggedIn = props.isLoggedIn;
     let setIsLoggedIn = props.setIsLoggedIn;
