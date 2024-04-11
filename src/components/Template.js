4e75d680579f685c5f455d15d0ed7c0e9dcfb5f1
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-11 23:32:28 +0530

    main

--- src/components/Template.js
+++ src/components/Template.js
@@ -34,7 +34,7 @@ const Template = ({title, desc1, desc2, image, formtype, setIsLoggedIn}) => {
             </div>
 
             <button className='w-full flex justify-center rounded-[8px] font-medium text-richblack-100
-            border border-richblack-700'>
+            border border-richblack-700 px-[12px] py-[8px] gap-x-2 mt-6'>
                 <FcGoogle/>
                 <p>Sign Up with Google</p>
             </button>
