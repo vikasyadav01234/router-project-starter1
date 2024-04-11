0210eb33e4addf6f906cdc97e4170b1d9bc2c399
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-11 23:55:50 +0530

    main

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -53,7 +53,7 @@ const LoginForm = ({setIsLoggedIn}) => {
 
         <label className='w-full'>
             <p className='text-[0.875rem] text-richblack-5 mb-1 leading-[1.375]'>
-                Password<sup>*</sup>
+                Password<sup className='text-pink-200'>*</sup>
             </p>
             <input 
                 required
