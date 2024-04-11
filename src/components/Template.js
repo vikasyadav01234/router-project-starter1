3861134ab7e06a05309e1a06cd4e01c73b815cc2
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-11 08:53:58 +0530

    main

--- src/components/Template.js
+++ src/components/Template.js
@@ -12,7 +12,7 @@ const Template = ({title, desc1, desc2, image, formtype, setIsLoggedIn}) => {
 
     <div className='flex w-11/12 max-w-[1160px] py-12 mx-auto gap-x-12 gap-y-0'>
 
-        <div>
+        <div className='w-11/12'>
             <h1>{title}</h1>
             <p>
                 <span>{desc1}</span>
