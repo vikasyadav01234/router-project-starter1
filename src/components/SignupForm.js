de52c4c0c8a9741f385ae6fd92321918d117e657
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-12 22:41:52 +0530

    main

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -117,7 +117,7 @@ const SignupForm = ({setIsLoggedIn}) => {
                         className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'
                     />
                     <span 
-                        className='absolute right-3 top-[38px] cursor-pointer'
+                        className='absolute right-3 top-[28px] cursor-pointer'
                     onClick={() => setShowPassword((prev) => !prev)}>
                         {showPassword ? (<AiOutlineEyeInvisible fontSize={24} fill='#AFB2BF'/>) :
                          (<AiOutlineEye fontSize={24} fill='#AFB2BF'/>)}
@@ -136,7 +136,7 @@ const SignupForm = ({setIsLoggedIn}) => {
                         className='bg-richblack-800 rounded-[0.5rem] text-richblack-5 w-full p-[12px]'
                     />
                     <span
-                        className='absolute right-3 top-[38px] cursor-pointer'
+                        className='absolute right-3 top-[60px] cursor-pointer'
                      onClick={() => setShowPassword((prev) => !prev)}>
                         {showPassword ? (<AiOutlineEyeInvisible fontSize={24} fill='#AFB2BF'/>) : 
                         (<AiOutlineEye fontSize={24} fill='#AFB2BF'/>)}
