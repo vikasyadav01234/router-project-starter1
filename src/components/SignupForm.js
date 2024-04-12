9d28d04d99c55bcc597c425347f844a38bb9cd2a
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-12 22:31:26 +0530

    main

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -137,7 +137,7 @@ const SignupForm = ({setIsLoggedIn}) => {
                     <span
                         className='absolute right-3 top-[38px] cursor-pointer'
                      onClick={() => setShowPassword((prev) => !prev)}>
-                        {showPassword ? (<AiOutlineEyeInvisible/>) : (<AiOutlineEye/>)}
+                        {showPassword ? (<AiOutlineEyeInvisible fontSize={24} fill='#AFB2BF'/>) : (<AiOutlineEye/>)}
                     </span>
                 </label>
             </div>
