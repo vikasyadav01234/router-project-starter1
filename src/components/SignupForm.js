0be814fc1f270d4e5be74f57727d29a76f93cff6
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 10:31:16 +0530

    main

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -63,7 +63,7 @@ const SignupForm = ({setIsLoggedIn}) => {
         <form onSubmit={submitHandler}>
         {/* first name and lastName */}
             <div className='flex gap-x-4 mt-4'>
-                    <label className='w-full mt-4'>
+                    <label className='w-full mt-[20px]'>
                         <p className='text-[0.875rem] text-richblack-5 mb-1 leading-[1.375]'>First Name<sup className='text-pink-600'>*</sup></p>
                         <input
                             required
