eed54af6c1b739524c4b2f05993e297d64a0c2ce
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 10:51:12 +0530

    main

--- src/App.js
+++ src/App.js
@@ -24,7 +24,11 @@ function App() {
         <Route path="/" element= {<Home/>} />
         <Route path="/login" element = {<Login  setIsLoggedIn={setIsLoggedIn} />} />
         <Route path="/signup" element={<Signup  setIsLoggedIn={setIsLoggedIn} />} />
-        <Route path="/dashboard" element = {<Dashboard/>} />
+        <Route path="/dashboard" element = {
+          <PrivateRoute>
+            <Dashboard/>
+          </PrivateRoute>
+        } />
 
       </Routes>
 
