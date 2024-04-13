9d5e63206e6885fafd8c6d50797043caec363452
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-04-13 10:48:18 +0530

    main

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -62,7 +62,7 @@ const SignupForm = ({setIsLoggedIn}) => {
             </button>
             <button
             className={`${accountType === "instructor" ? "bg-richblack-900 text-richblack-5"
-            :"bg-transparent text-richblack-200"}`}
+            :"bg-transparent text-richblack-200"} py-2 px-5 rounded-full transition-all duration-200`}
             onClick={()=>setAccountType("instructor")}
             >
                 Instructor
